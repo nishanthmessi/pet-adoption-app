b66a9f41357cc3de31ff1400cbd47622cff02eb3
Nishanth <nisanthrtts@gmail.com>
2023-09-07 09:46:28 +0530

    server url updated

--- client/src/features/slices/apiSlice.js
+++ client/src/features/slices/apiSlice.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' })
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'https://top-hat-gopher.cyclic.app',
+})
 
 export const apiSlice = createApi({
   baseQuery,
